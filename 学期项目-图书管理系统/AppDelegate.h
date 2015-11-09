b71636b35ad8a8620f348e2fd6ca73922284842d
MA LEI <apple@APPLEdeMacBook-Pro-46.local>
2015-11-09 16:04:28 +0800

    Initial Commit

---
+++ 学期项目-图书管理系统/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  学期项目-图书管理系统
+//
+//  Created by APPLE on 15/11/9.
+//  Copyright (c) 2015年 big nerd ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 学期项目-图书管理系统/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  学期项目-图书管理系统
+//
+//  Created by APPLE on 15/11/9.
+//  Copyright (c) 2015年 big nerd ranch. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
